f4f5f04d5f37351d2d9ac1eb07853c0e61b6f0b3
Daniel-byte628 <danigutierritos2817@gmail.com>
2023-07-20 23:48:39 -0500

    Lombok, Matriz y clases necesarias

--- src/main/java/kakuro/kakuro/HelloApplication.java
+++ src/main/java/kakuro/kakuro/HelloApplication.java
@@ -2,16 +2,30 @@ package kakuro.kakuro;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import kakuro.kakuro.Modelo.Tablero;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/Tablero8.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 620, 420);
+
+        Tablero tablero = new Tablero();
+        tablero.mostrarMatriz();
+
+        // Cargar el archivo FXML y crear la escena
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/Menu.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 1220, 920);
+
+        // Agregar el archivo CSS a la escena
+        String css = this.getClass().getResource("LetraMenu.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        // Configurar la escena y mostrar la ventana
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module kakuro.kakuro {
     requires javafx.controls;
     requires javafx.fxml;
+    requires lombok;
 
 
     opens kakuro.kakuro to javafx.fxml;
-
-
     exports kakuro.kakuro;
+    exports kakuro.kakuro.Controlador;
+    opens kakuro.kakuro.Controlador to javafx.fxml;
+
 }
